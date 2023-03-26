0378f000c9a5f49848a3f75c327ab2cba11fa1ee
Vitaly <Vitalylevitin2@gmail.com>
2023-03-26 22:29:26 +0300

    Completed Auth

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using HomeAssignment.Data;
 using HomeAssignment.Services.AssignmentService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Pomelo.EntityFrameworkCore.MySql;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,9 +16,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<IAssignmentService, AssignmentService>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+
+//Felt like this is too much so gave up on it. Too much of a hassle imo.
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(options =>
+//     {
+//         options.TokenValidationParameters = new TokenValidationParameters{
+//             ValidateIssuerSigningKey = true,
+//             IssuerSigningKey = new SymmetricSecurityKey
+//                 (System.Text.Encoding.UTF8.GetBytes
+//                 (builder.Configuration.GetSection("AppSettings:Token").Value!)),
+//                 ValidateIssuer = false,
+//                 ValidateAudience = false
+//         };
+//     });
 
 var app = builder.Build();
 
@@ -28,6 +45,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// app.UseAuthentication();
 
 app.UseAuthorization();
 
